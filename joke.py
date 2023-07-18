5683c8f14d13a74c02908d0a29fb8d4a89073b10
nolson307 <s3732752@students.fscj.edu>
2023-07-17 19:33:40 -0700

    uploding joke.py!

---
+++ joke.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://icanhazdadjoke.com/"
+
+payload = {}
+headers = {
+	'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
